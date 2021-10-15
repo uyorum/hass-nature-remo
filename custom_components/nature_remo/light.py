c77a2431df6738ce67b65501338a2da264b28c37
mrtolkien <gary.mialaret@gmail.com>
2021-10-15 11:39:52 +0900

    Trying to add a requirement and basic code

--- custom_components/nature_remo/light.py
+++ custom_components/nature_remo/light.py
@@ -4,14 +4,15 @@ from __future__ import annotations
 import logging
 from typing import Any
 
-from homeassistant.core import callback
-from homeassistant.components.light import LightEntity, PLATFORM_SCHEMA
+from homeassistant.components.light import LightEntity
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from . import DOMAIN, NatureRemoBase
+
+import remo
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -46,16 +47,54 @@ async def async_setup_platform(
 class NatureRemoLight(NatureRemoBase, LightEntity):
     """Implementation of a Nature Remo light"""
 
-    def turn_off(self, **kwargs: Any) -> None:
-        pass
-
-    def turn_on(self, **kwargs: Any) -> None:
-        pass
-
     def __init__(self, coordinator, api, appliance, config):
         super().__init__(coordinator, appliance)
 
+        # TODO Use the right python package instead
         self._api = api
 
-        _LOGGER.error(appliance)
+        if appliance["state"]["power"] == "on":
+            self._state = True
+        elif appliance["state"]["power"] == "off":
+            self._state = True
+        else:
+            self._state = None
+
+        _LOGGER.error(appliance["state"])
         _LOGGER.error(config)
+
+    # @property
+    # def brightness(self):
+    #     """Return the brightness of the light.
+    #     This method is optional. Removing it indicates to Home Assistant
+    #     that brightness is not supported for this light.
+    #     """
+    #     return self._brightness
+
+    @property
+    def is_on(self) -> bool | None:
+        """Return true if light is on."""
+        return self._state
+
+    def turn_on(self, **kwargs: Any) -> None:
+        """Instruct the light to turn on.
+        You can skip the brightness part if your light does not support
+        brightness control.
+        """
+        return None
+        # self._light.brightness = kwargs.get(ATTR_BRIGHTNESS, 255)
+        # self._light.turn_on()
+
+    def turn_off(self, **kwargs: Any) -> None:
+        """Instruct the light to turn off."""
+        return None
+        # self._light.turn_off()
+
+    def update(self) -> None:
+        """Fetch new state data for this light.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        return None
+        # self._light.update()
+        # self._state = self._light.is_on()
+        # self._brightness = self._light.brightness
