24979f5630d2848983ff85886e5266fff4ebb751
mrtolkien <gary.mialaret@gmail.com>
2021-10-21 12:08:53 +0900

    Added binary motion sensor.py

--- custom_components/nature_remo/sensor.py
+++ custom_components/nature_remo/sensor.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType, StateType
@@ -14,21 +15,21 @@ from homeassistant.components.sensor import SensorEntity
 
 from . import DOMAIN, _LOGGER
 
-# TODO BinarySensor for motion
 
-# TODO Use the values from Home Assistant
-#   Move the data out in its own class too
+# TODO Move the data out in its own class too
 sensor_class = {
     "hu": "humidity",
     "il": "illuminance",
-    # "mo": "motion", # TODO
     "te": "temperature",
+}
+
+binary_sensor_class = {
+    "mo": "motion",
 }
 
 sensor_units = {
     "hu": "%",
-    "il": "lm",  # TODO Check
-    "mo": "?",
+    "il": "lm",
     "te": "°C",
 }
 
@@ -60,6 +61,63 @@ async def async_setup_platform(
             if sensor_type in sensor_class
         ]
     )
+
+    async_add_entities(
+        [
+            NatureRemoBinarySensor(
+                devices_update_coordinator,
+                device,
+                sensor_type,
+            )
+            for device in devices_update_coordinator.data.values()
+            for sensor_type in device["newest_events"]
+            if sensor_type in binary_sensor_class
+        ]
+    )
+
+
+# TODO Common code with NatureRemoSensor
+class NatureRemoBinarySensor(CoordinatorEntity, BinarySensorEntity):
+    def __init__(
+        self,
+        coordinator: DataUpdateCoordinator,
+        device: dict,
+        sensor_type: str,
+    ):
+        # We don't need to have an API object in that one as the updating is fully done through the coordinator
+
+        # Calling the CoordinatorEntity constructor which adds listeners and self.coordinator
+        super(NatureRemoBinarySensor, self).__init__(coordinator)
+
+        # Concatenating the device name and sensor type
+        self._name = f"{device['name']} - {sensor_class[sensor_type]}"
+
+        # device.id cannot unique id in itself as there are multiple sensors per device
+        self._device_id = device["id"]
+        self._sensor_type = sensor_type
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def unique_id(self) -> str | None:
+        return f"{self._device_id}-{self._sensor_type}"
+
+    @property
+    def device_class(self) -> str | None:
+        return binary_sensor_class[self._sensor_type]
+
+    @property
+    def is_on(self):
+        val = self.coordinator.data[self._device_id]["newest_events"][
+            self._sensor_type
+        ]["val"]
+
+        if val > 0:
+            return True
+        else:
+            return False
 
 
 class NatureRemoSensor(CoordinatorEntity, SensorEntity):
